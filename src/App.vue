c10a9b6dac8fa0562c84a9fcf8baf39e9da0f4e1
Carl Hain <crhain@gmail.com>
2020-03-23 17:53:08 -0500

    Add logic to App that hooks up events passed from Answer and Question components

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,11 @@
     <hr />
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <component :is="mode"></component>
+        <component
+          :is="mode"
+          @answered="answered($event)"
+          @confirmed="mode = 'app-question'"
+        ></component>
       </div>
     </div>
   </div>
@@ -27,6 +31,16 @@ export default {
   components: {
     appQuestion: Question,
     appAnswer: Answer
+  },
+  methods: {
+    answered(isCorrect) {
+      if (isCorrect) {
+        this.mode = "app-answer";
+      } else {
+        this.mode = "app-question";
+        alert("Wrong! Try again fool!");
+      }
+    }
   }
 };
 </script>
